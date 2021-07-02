965a94281cc241c88b3634fd4705e1e74a05e7d9
waveay <dominik.rene.danner@gmail.com>
2021-07-02 16:38:08 +0200

    #12 v1.8.5 Bug Fixes » Docs

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# YVC - Youtube Video Converter
-Opensource Youtube Video Converter
+# Getting Started with Create React App
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm run server`
+
+Runs the websocket and the rest-api
+Server uses `Port 4000 and 5000`.
+
+Its possible that the server will have some problems.\
+You will also see any lint errors in the console.
+
+### `npm run client`
+
+Launches React App in `Dev Mode`.\
+Run `npm run build` in the /client directory to build the project.
+
+### `npm run app`
+
+Runs the Electron Instance in Development mode.\
+This electron app runs a proxy and depends on the react app.
+For Production build the project by using the instruction on the [Electron Builder Page](https://www.electron.build/).
+
+The build is minified and the filenames include the hashes.
--- app/src/index.js
+++ app/src/index.js
@@ -1,35 +1,25 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 600,
-    height: 700,
+    width: 700,
+    height: 800,
     minWidth: 600,
     minHeight: 700,
   });
 
   mainWindow.removeMenu()
-  mainWindow.loadURL('http://192.168.0.27:3000')
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  mainWindow.loadURL('http://localhost:3000')
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -37,12 +27,7 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
